94841780f4f1fc9fd94110006421382cda8148c5
Amitansh Gangwar <amitansh.g.aux@go-jek.com>
2018-05-22 16:10:05 +0530

    Changed Variables

--- server.go
+++ server.go
@@ -11,28 +11,26 @@ func main() {
     
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		if r.Method=="POST"{
-			request:=r.URL.Path[5:]
-			i:=0
-			a:=""
-			b:=""
-			for ;request[i]<='9' && request[i]>='0';{
-				a=a+string(request[i])
-				i++
+			requestMessage:=r.URL.Path[5:]
+			index:=0
+			firstNumber:=""
+			secondNumber:=""
+			for ;requestMessage[index]<='9' && requestMessage[index]>='0';{
+				firstNumber=firstNumber+string(requestMessage[index])
+				index++
 			}
-			i++
-			for ;i<len(request) && request[i]<='9' && request[i]>='0';{
-				b=b+string(request[i])
-				i++
+			index++
+			for ;index<len(requestMessage) && requestMessage[index]<='9' && requestMessage[index]>='0';{
+				secondNumber=secondNumber+string(requestMessage[index])
+				index++
 			}
-			fmt.Println(a)
-			fmt.Println(b)
-			//a:=request[5:6]
-			ai,_:=strconv.Atoi(a)
-			//b:=request[7:8]
-			bi,_:=strconv.Atoi(b)
-			ci:=ai+bi
-			c:=strconv.Itoa(ci)
-			fmt.Fprintf(w, c)
+			
+			firstInteger,_:=strconv.Atoi(firstNumber)
+			
+			secondInteger,_:=strconv.Atoi(secondNumber)
+			sum:=firstInteger+secondInteger
+			sumString:=strconv.Itoa(sum)
+			fmt.Fprintf(w, sumString)
 		}
     })
 
