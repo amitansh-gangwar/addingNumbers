5b614904636fe4bf5fa3ea12c2ffe505a8a5d0b8
Amitansh Gangwar <amitansh.g.aux@go-jek.com>
2018-05-23 15:04:19 +0530

    Multiplying by header value

--- server.go
+++ server.go
@@ -10,7 +10,7 @@ import (
 )
 
 type solution struct{
-	Addition int
+	Result int
 	Success string
 }
 
@@ -18,13 +18,14 @@ func main() {
     
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		if r.Method == "POST"{
-			errortype:=""
+			
 			requestMessage := r.URL.Path[5:]
 			
 			var numbers []string
 			numbers = strings.Split(requestMessage,"/")
-			
+
 			sum := 0
+			errortype:=""
 			for _,number := range numbers{
 				integer,error := strconv.Atoi(number)
 				if error!=nil {
@@ -34,12 +35,17 @@ func main() {
 				sum = sum + integer
 			}
 
+			var multiplyBy int
+			multiplyBy,_=strconv.Atoi(r.Header.Get("multiply"))
+			
+			multiplication := sum * multiplyBy
+
 			if len(requestMessage)==0 {
 				fmt.Fprintf(w,"No integers given as input")
 			}else if errortype=="type error" {
 				fmt.Fprintf(w,"Entered numbers must be of type int")	
 			}else{
-				answer := solution{sum,"ok"}
+				answer := solution{multiplication,"ok"}
 				resultJson,_ := json.Marshal(answer)
 				ans := string(resultJson)
 				fmt.Fprintf(w,ans)
